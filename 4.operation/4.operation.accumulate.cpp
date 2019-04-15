9f4b115eceb028e6b1abe76a1d0413992136189a
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2019-04-15 15:52:31 +0800

    +4.operation.accumulate

---
+++ 4.operation/4.operation.accumulate.cpp
@@ -0,0 +1,13 @@
+
+int accumulation(int nNumber) {
+    // nNumber && (nNumber += Accumulation(nNumber - 1));
+
+    (nNumber == 0) || (nNumber += accumulation(nNumber - 1));
+    return  nNumber;
+}
+
+
+int main(void){
+    accumulation (33);
+    return 0;
+}
